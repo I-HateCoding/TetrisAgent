b4ae5b80da2766a02d9762fc3063e88705c4854c
I-HateCoding <154570015+I-HateCoding@users.noreply.github.com>
2026-06-12 21:33:07 +0800

    Initial commit

---
+++ tetris/main.py
@@ -0,0 +1,14 @@
+import cv2
+import gymnasium as gym
+from tetris_gymnasium.envs.tetris import Tetris
+
+env = gym.make("tetris_gymnasium/Tetris", render_mode="human")
+env.reset(seed=42)
+
+terminated = False
+while not terminated:
+    env.render()
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    key= cv2.waitKey(100)
+print("Game Over!")
